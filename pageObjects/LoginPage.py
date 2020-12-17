785afca87b0503e0f676d5832218b974daae2902
ketan-Cyperts <ketanrathod.cyperts>
2020-12-17 20:01:21 +0530

    Commt

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -9,11 +9,11 @@ class LoginPage:
 
     def setUserName(self, username):
         self.driver.find_elemnet_by_id(self.textbox_username_id).clear()
-        self.driver.find_elemnet_by_id(self.textbox_username_id).send_keys("username")
+        self.driver.find_elemnet_by_id(self.textbox_username_id).send_keys(username)
 
     def setPassword(self, password):
         self.driver.find_elemnet_by_id(self.textbox_password_id).clear()
-        self.driver.find_elemnet_by_id(self.textbox_password_id).send_keys("password")
+        self.driver.find_elemnet_by_id(self.textbox_password_id).send_keys(password)
 
     def clickLogin(self):
         self.driver.find_elemnet_by_xpath(self.button_login_Xpath).click()
@@ -21,5 +21,4 @@ class LoginPage:
     def clickLogout(self):
         self.driver.find_elemnet_by_link_text(self.link_logout_linktext).click()
 
-asdd
 
