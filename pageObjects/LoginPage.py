87e071461eba27d80d1305a18216ef52685f563a
ketan-Cyperts <ketanrathod.cyperts>
2020-12-23 22:39:59 +0530

    asdasd

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+
+
 class LoginPage:
     textbox_username_id = "Email"
     textbox_password_id = "Password"
@@ -7,18 +10,21 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def setUserName(self, username):
-        self.driver.find_elemnet_by_id(self.textbox_username_id).clear()
-        self.driver.find_elemnet_by_id(self.textbox_username_id).send_keys(username)
+    def setUserName(self,username):
+        self.driver.find_element_by_id(self.textbox_username_id).clear()
+        self.driver.find_element_by_id(self.textbox_username_id).send_keys(username)
+    # def setUserName(self, username):
+    #     self.driver.find_elemnet_by_id(self.textbox_username_id).clear()
+    #     self.driver.find_elemnet_by_id(self.textbox_username_id).send_keys(username)
 
     def setPassword(self, password):
-        self.driver.find_elemnet_by_id(self.textbox_password_id).clear()
-        self.driver.find_elemnet_by_id(self.textbox_password_id).send_keys(password)
+        self.driver.find_element_by_id(self.textbox_password_id).clear()
+        self.driver.find_element_by_id(self.textbox_password_id).send_keys(password)
 
     def clickLogin(self):
-        self.driver.find_elemnet_by_xpath(self.button_login_Xpath).click()
+        self.driver.find_element_by_xpath(self.button_login_Xpath).click()
 
     def clickLogout(self):
-        self.driver.find_elemnet_by_link_text(self.link_logout_linktext).click()
+        self.driver.find_element_by_link_text(self.link_logout_linktext).click()
 
 
--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,36 +1,55 @@
 import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
+from utilities.readProperties import ReadConfig
+from utilities.customerLogger import LogGen
+import logging
+# logging.basicConfig(filename="./Logs.log", format='%(asctime)s:%(levelname)s:%(message)s',level=logging.INFO)
+#
+# logging.info("Ketan")
 
-class Test_001_Login():
-    baseurl = "https://admin-demo.nopcommerce.com/"
-    username = "admin@yourstore.com"
-    password = "admin"
+class Test_001_Login:
+    baseurl = ReadConfig.getApplicationurl()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
 
-    def test_homePageTitle(self, setup):
+    logger = LogGen.loggen()
+
+
+    def test_homePageTitle(self,setup):
+        self.logger.info("*********Test_001_Login*********")
+        self.logger.info("*********test_homePageTitle Started*********")
         self.driver = setup
         self.driver.get(self.baseurl)
         act_title = self.driver.title
         if act_title == "Your store. Login":
             self.driver.close()
+            self.logger.info("*********test_homePageTitle Test is Passed*********")
             assert True
         else:
+            self.driver.save_screenshot(".\\Screenshots\\"+"test_homePageTitle.png")
             self.driver.close()
+            self.logger.info("*********test_homePageTitle Test is Failed*********")
             assert False
 
     def test_login(self, setup):
+        self.logger.info("*********test_login Started*********")
         self.driver = setup
         self.driver.get(self.baseurl)
         self.lp = LoginPage(self.driver)
-       # self.lp.setUserName(self.username)
-      #  self.lp.setPassword(self.password)
+        self.driver.get(self.baseurl)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
         self.lp.clickLogin()
         act_title = self.driver.title
         if act_title == "Dashboard / nopCommerce administration":
             self.driver.close()
+            self.logger.info("*********test_login Test is Passed*********")
             assert True
         else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_login.png")
             self.driver.close()
+            self.logger.info("*********test_login Test is Failed*********")
             assert False
 
 
